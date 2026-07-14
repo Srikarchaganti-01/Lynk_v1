c05b19de2f2412b36b8390417f8504eb898d9afc
Srikarchaganti-01 <srikarchaganti134@gmail.com>
2026-07-14 23:02:11 +0530

    removing trash files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 class="text-3xl font-bold underline text-amber-300">Hello world!</h1>
+      <div className="min-h-screen bg-black text-white flex items-center justify-center">
+        <h1 className="text-5xl font-bold">Lynk</h1>
+      </div>
     </>
   );
 }
